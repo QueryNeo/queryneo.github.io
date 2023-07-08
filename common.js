6a337bd10bec328f6b13243b31a5c9b89706f52a
QueryNeo <matthewtfsevern@gmail.com>
2023-07-08 12:42:20 +0800

    i did it! music boxes and also .js file added

--- common.js
+++ common.js
@@ -1,5 +1,40 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> 
 $(function(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
   });
+
+   //stolen from https://stackoverflow.com/questions/8489710/play-an-audio-file-using-jquery-when-a-button-is-clicked 
+  $(document).ready(function() {
+    var audioElement = document.createElement('audio');
+
+    if (audioPath != null){
+    audioElement.setAttribute('src', audioPath);
+    
+    audioElement.addEventListener('ended', function() { 
+      this.play();
+    }, false);
+    
+    audioElement.addEventListener("canplay",function(){
+        $("#length").text("Duration:" + audioElement.duration + " seconds");
+        $("#source").text("Source:" + audioElement.src);
+        $("#status").text("Status: Ready to play").css("color","green");
+    });
+    
+    audioElement.addEventListener("timeupdate",function(){
+        $("#currentTime").text("Current second:" + audioElement.currentTime);
+    });
+    
+    $('#play').click(function() {
+        audioElement.play();
+        $("#status").text("Status: Playing");
+    });
+    
+    $('#pause').click(function() {
+        audioElement.pause();
+        $("#status").text("Status: Paused");
+    });
+    
+    $('#restart').click(function() {
+        audioElement.currentTime = 0;
+    });
+}});
